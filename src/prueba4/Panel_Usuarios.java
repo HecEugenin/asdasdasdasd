4992f4984a974decdb423775b2bb71bf1a47b7cf
yerico <yerico@yerico-PC>
2015-12-11 04:14:35 -0300

    11-12-2015 4:14

--- src/prueba4/Panel_Usuarios.java
+++ src/prueba4/Panel_Usuarios.java
@@ -18,10 +18,22 @@ public class Panel_Usuarios extends JPanel {
 
     JLabel lblNombre = new JLabel("Nombre:");
     JTextField txtNombre = new JTextField();
-    JLabel lblApellido = new JLabel("Apellido");
-    JTextField txtApellido = new JTextField();
-    JLabel lblCiudad = new JLabel("Ciudad");
-    JTextField txtCiudad = new JTextField();
+    
+    JLabel lblId_Usuarios = new JLabel("id_usuarios");
+    JTextField txtId_Usuarios = new JTextField();
+    
+    JLabel lblRut = new JLabel("RUT");
+    JTextField txtRut = new JTextField();
+    
+    JLabel lblApellidoPAT = new JLabel("Apellido Paterno");
+    JTextField txtApellidoPAT = new JTextField();
+    
+    JLabel lblApellidoMAT = new JLabel("Apellido Materno");
+    JTextField txtApellidoMAT = new JTextField();
+    
+    JLabel lblId_Mascota = new JLabel("id_mascota");
+    JTextField txtId_Mascota = new JTextField();
+    
     JButton btnAgregar = new JButton("Agregar");
     JButton btnEliminar = new JButton("ELiminar");
     JButton btnEditar = new JButton("Editar");
@@ -29,25 +41,44 @@ public class Panel_Usuarios extends JPanel {
 
     public Panel_Usuarios() {
         this.setLayout(null);
+        
         lblNombre.setBounds(20, 20, 120, 20);
         this.add(lblNombre);
         txtNombre.setBounds(150, 20, 120, 20);
         this.add(txtNombre);
-        lblApellido.setBounds(20, 50, 120, 20);
-        this.add(lblApellido);
-        txtApellido.setBounds(150, 50, 120, 20);
-        this.add(txtApellido);
-        lblCiudad.setBounds(20, 80, 120, 20);
-        this.add(lblCiudad);
-        txtCiudad.setBounds(150, 80, 120, 20);
-        this.add(txtCiudad);
-        btnAgregar.setBounds(20, 110, 120, 20);
+        
+        lblRut.setBounds(20, 50, 120, 20);
+        this.add(lblRut);
+        txtRut.setBounds(150, 50, 120, 20);
+        this.add(txtRut);
+        
+        lblApellidoPAT.setBounds(20, 80, 120, 20);
+        this.add(lblApellidoPAT);
+        txtApellidoPAT.setBounds(150, 80, 120, 20);
+        this.add(txtApellidoPAT);
+        
+        lblApellidoMAT.setBounds(20, 110, 120, 20);
+        this.add(lblApellidoMAT);
+        txtApellidoMAT.setBounds(150, 110, 120, 20);
+        this.add(txtApellidoMAT);
+        
+        lblId_Mascota.setBounds(20, 140, 120, 20);
+        this.add(lblId_Mascota);
+        txtId_Mascota.setBounds(150, 140, 120, 20);
+        this.add(txtId_Mascota);
+        
+       lblId_Usuarios.setBounds(280, 140, 120, 20);
+        this.add(lblId_Usuarios);
+        txtId_Usuarios.setBounds(360, 140, 120, 20);
+        this.add(txtId_Usuarios);
+        
+        btnAgregar.setBounds(20, 180, 120, 20);
         this.add(btnAgregar);
-        btnLimpiar.setBounds(150, 110, 120, 20);
+        btnLimpiar.setBounds(150, 180, 120, 20);
         this.add(btnLimpiar);
-        btnEliminar.setBounds(280, 110, 120, 20);
+        btnEliminar.setBounds(280, 180, 120, 20);
         this.add(btnEliminar);
-        btnEditar.setBounds(410, 110, 120, 20);
+        btnEditar.setBounds(410, 180, 120, 20);
         this.add(btnEditar);
         OyenteLimpiar oLimpiar = new OyenteLimpiar();
         btnLimpiar.addActionListener(oLimpiar);
@@ -68,11 +99,20 @@ public class Panel_Usuarios extends JPanel {
         public void actionPerformed(ActionEvent e) {
             Usuarios usu = new Usuarios();
             String nom = txtNombre.getText();
-            String ape = txtApellido.getText();
-            int Mascota_id = Integer.parseInt(txtCiudad.getText());
+            int Id_Usu= Integer.parseInt(txtId_Usuarios.getText());
+            int rut= Integer.parseInt(txtRut.getText());
+            String apeP = txtApellidoPAT.getText();
+            String apeM = txtApellidoMAT.getText();
+            int Mascota_id = Integer.parseInt(txtId_Mascota.getText());
+            
+            
+            
 
             usu.setNombre(nom);
-            usu.setApepat(ape);
+            usu.setId_Usuario(Id_Usu);
+            usu.setRut(rut);
+            usu.setApepat(apeP);
+            usu.setApemat(apeM);
             usu.setId_mascota(Mascota_id);
             usu.save();
             Limpiar();
@@ -81,8 +121,12 @@ public class Panel_Usuarios extends JPanel {
     
     public void Limpiar() {
         txtNombre.setText("");
-        txtApellido.setText("");
-        txtCiudad.setText("");
+        txtId_Usuarios.setText("");
+        txtRut.setText("");
+        txtApellidoPAT.setText("");
+        txtApellidoMAT.setText("");
+        txtId_Mascota.setText("");
+        
     }
 
 }
